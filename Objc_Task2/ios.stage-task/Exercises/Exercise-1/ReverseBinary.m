f1fa9f11aa97b59218e1f8fea95b2f45f6719602
Dzmitry Ilyin <Dzmitry_Ilyin@epam.com>
2021-05-16 13:58:11 +0300

    Exercise2_1 solved

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,16 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSUInteger reversedBinary = 0;
+    for (NSUInteger index = 0; index < 8; index++)
+    {
+        if (n/2 >= 0) {
+            if(n%2){
+                reversedBinary += pow(2, 7 - index);
+            }
+            n /= 2;
+        } else break;
+    }
+    
+    return reversedBinary;
 }
